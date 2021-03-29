ad4b9b05713f7b57ead6228628d35715cfeccbba
RomainGe <58071634+RomainGe@users.noreply.github.com>
2021-03-29 15:00:50 +0200

    Add icons on card

--- components/star/Card.vue
+++ components/star/Card.vue
@@ -1,12 +1,20 @@
 <template>
     <div class="w-full max-w-sm overflow-hidden rounded border bg-white shadow">
       <div class="p-3">
-        <svg @click="addFavorite()" v-if="!star.isFavorite" class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-        </svg>
-        <svg @click="removeFavorite()" v-else class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-          <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
-        </svg>
+        <div class="grid grid-cols-2 gap-4">
+          <svg @click="addFavorite()" v-if="!star.isFavorite" class="h-10 w-10 col-span-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+          </svg>
+          <svg @click="removeFavorite()" v-else class="h-10 w-10 col-span-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="red">
+            <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
+          </svg>
+          <svg class="h-20 w-20 col-span-1" v-if="!selectIcon()" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="grey">
+            <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+          </svg>
+          <svg class="h-20 w-20 col-span-1" v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="lightblue">
+            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM4.332 8.027a6.012 6.012 0 011.912-2.706C6.512 5.73 6.974 6 7.5 6A1.5 1.5 0 019 7.5V8a2 2 0 004 0 2 2 0 011.523-1.943A5.977 5.977 0 0116 10c0 .34-.028.675-.083 1H15a2 2 0 00-2 2v2.197A5.973 5.973 0 0110 16v-2a2 2 0 00-2-2 2 2 0 01-2-2 2 2 0 00-1.668-1.973z" clip-rule="evenodd" />
+          </svg>
+        </div>
           <div class="flex items-start justify-between grid grid-cols-2 gap-4">
               <p class="text-xs text-gray-500 p-2">Name : </p>
               <p class="text-lg">{{star.name}}</p>
@@ -63,6 +71,13 @@ export default Vue.extend({
           this.$toasted.error('Failed :/ , try again please !').goAway(1500);
         }
       },
+      selectIcon(){
+        if(this.star.isPlanet){
+          return true;
+        }else{
+          return false;
+        }
+      }
     },
     computed: {
     },
--- pages/starPage/_id.vue
+++ pages/starPage/_id.vue
@@ -1,106 +1,106 @@
 <template>
-    <div class="w-full max-w-sm bg-white">
+    <div class="back-planet h-screen text-white content-center">
       <NuxtLink class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-10 mx-10" to="/" >Go back</NuxtLink >
       <div class="p-3">
           <div class="flex items-start justify-between grid grid-cols-2 gap-6">
               <div class="grid grid-cols-2 gap-4">
                 <div class="grid grid-cols-2 gap-4 col-span-2">
-                  <p class="text-xs text-gray-500 p-2">FrenchName :</p>
+                  <p class="text-xs text-white p-2">FrenchName :</p>
                   <p class="text-lg">{{star.name}}</p>
                 </div>
                 <div class="grid grid-cols-2 gap-4 col-span-2">
-                  <p class="text-xs text-gray-500">EnglishName :</p>
+                  <p class="text-xs text-white">EnglishName :</p>
                   <p class="text-lg">{{star.englishName}}</p>
                 </div>
                 <div class="grid grid-cols-2 gap-4 col-span-2">
-                  <p class="text-xs text-gray-500">Density : </p>
+                  <p class="text-xs text-white">Density : </p>
                   <p class="text-lg">{{star.density}}</p>
                 </div>
                 <div class="grid grid-cols-2 gap-4 col-span-2">
-                  <p class="text-xs text-gray-500">Gravity : </p>
+                  <p class="text-xs text-white">Gravity : </p>
                   <p class="text-lg">{{star.gravity}}</p>
                 </div>
                 <div class="grid grid-cols-2 gap-4 col-span-2">
-                  <p class="text-xs text-gray-500">semimajorAxis : </p>
+                  <p class="text-xs text-white">semimajorAxis : </p>
                   <p class="text-lg">{{star.semimajorAxis}}</p>
                 </div>
                 <div class="grid grid-cols-2 gap-4 col-span-2">
-                  <p class="text-xs text-gray-500">perihelion : </p>
+                  <p class="text-xs text-white">perihelion : </p>
                   <p class="text-lg">{{star.perihelion}}</p>
                 </div>
                 <div class="grid grid-cols-2 gap-4 col-span-2">
-                  <p class="text-xs text-gray-500">aphelion : </p>
+                  <p class="text-xs text-white">aphelion : </p>
                   <p class="text-lg">{{star.aphelion}}</p>
                 </div>
                 <div class="grid grid-cols-2 gap-4 col-span-2">
-                  <p class="text-xs text-gray-500">eccentricity : </p>
+                  <p class="text-xs text-white">eccentricity : </p>
                   <p class="text-lg">{{star.eccentricity}}</p>
                 </div>
                 <div class="grid grid-cols-2 gap-4 col-span-2">
-                  <p class="text-xs text-gray-500">inclination : </p>
+                  <p class="text-xs text-white">inclination : </p>
                   <p class="text-lg">{{star.inclination}}</p>
                 </div>
                 <div class="grid grid-cols-2 gap-4 col-span-2">
-                  <p class="text-xs text-gray-500">escape : </p>
+                  <p class="text-xs text-white">escape : </p>
                   <p class="text-lg">{{star.escape}}</p>
                 </div>
               </div>
             <div class="grid grid-cols-2 gap-4">
               <div class="grid grid-cols-2 gap-4 col-span-2">
-                <p class="text-xs text-gray-500">meanRadius : </p>
+                <p class="text-xs text-white">meanRadius : </p>
                 <p class="text-lg">{{star.meanRadius}}</p>
               </div>
               <div class="grid grid-cols-2 gap-4 col-span-2">
-                <p class="text-xs text-gray-500">equaRadius : </p>
+                <p class="text-xs text-white">equaRadius : </p>
                 <p class="text-lg">{{star.equaRadius}}</p>
               </div>
               <div class="grid grid-cols-2 gap-4 col-span-2">
-                <p class="text-xs text-gray-500">polarRadius : </p>
+                <p class="text-xs text-white">polarRadius : </p>
                 <p class="text-lg">{{star.polarRadius}}</p>
               </div>
               <div class="grid grid-cols-2 gap-4 col-span-2">
-                <p class="text-xs text-gray-500">flattening : </p>
+                <p class="text-xs text-white">flattening : </p>
                 <p class="text-lg">{{star.flattening}}</p>
               </div>
               <div class="grid grid-cols-2 gap-4 col-span-2">
-                <p class="text-xs text-gray-500">dimension : </p>
+                <p class="text-xs text-white">dimension : </p>
                 <p class="text-lg">{{star.dimension}}</p>
               </div>
               <div class="grid grid-cols-2 gap-4 col-span-2">
-                <p class="text-xs text-gray-500">sideralOrbit : </p>
+                <p class="text-xs text-white">sideralOrbit : </p>
                 <p class="text-lg">{{star.sideralOrbit}}</p>
               </div>
               <div class="grid grid-cols-2 gap-4 col-span-2">
-                <p class="text-xs text-gray-500">sideralRotation : </p>
+                <p class="text-xs text-white">sideralRotation : </p>
                 <p class="text-lg"> {{star.sideralRotation}}</p>
               </div>
               <div class="grid grid-cols-2 gap-4 col-span-2">
-                <p class="text-xs text-gray-500">discoveredBy : </p>
+                <p class="text-xs text-white">discoveredBy : </p>
                 <p class="text-lg">{{star.discoveredBy}}</p>
               </div>
               <div class="grid grid-cols-2 gap-4 col-span-2">
-                <p class="text-xs text-gray-500">discoveryDate : </p>
+                <p class="text-xs text-white">discoveryDate : </p>
                 <p class="text-lg">{{star.discoveryDate}}</p>
               </div>
               <div class="grid grid-cols-2 gap-4 col-span-2">
-                <p class="text-xs text-gray-500">alternativeName : </p>
+                <p class="text-xs text-white">alternativeName : </p>
                 <p class="text-lg">{{star.alternativeName}}</p>
               </div>
               <div class="grid grid-cols-2 gap-4 col-span-2">
-                <p class="text-xs text-gray-500">axialTilt :</p>
+                <p class="text-xs text-white">axialTilt :</p>
                 <p class="text-lg">{{star.axialTilt}}</p>
               </div>
               <div class="grid grid-cols-2 gap-4 col-span-2">
-                <p class="text-xs text-gray-500">Mass : </p>
+                <p class="text-xs text-white">Mass : </p>
               </div>
               <div class="grid grid-cols-2 gap-4 col-span-2">
-                <p class="text-xs text-gray-500">massExponent</p>
+                <p class="text-xs text-white">massExponent</p>
               </div>
               <div class="grid grid-cols-2 gap-4 col-span-2">
-                <p class="text-xs text-gray-500">Vol : </p>
+                <p class="text-xs text-white">Vol : </p>
               </div>
               <div class="grid grid-cols-2 gap-4 col-span-2">
-                <p class="text-xs text-gray-500">VolExponent</p>
+                <p class="text-xs text-white">VolExponent</p>
               </div>
             </div>
 
@@ -133,5 +133,11 @@ export default Vue.extend({
 })
 </script>
 <style>
-
+.back-planet{
+  background: url('assets/background-planet.jpg') no-repeat center center fixed; 
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+}
 </style>
